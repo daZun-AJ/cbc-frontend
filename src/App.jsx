ecd43457fc346bb437e38a9254c99f61c91774f5
Dasun <dasuna39@gmail.com>
2025-06-21 14:16:54 +0530

    Add login and registration pages with basic styling ➕

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
+import LoginPage from './pages/loginPage'
+import RegisterPage from './pages/registerPage'
 
 function App() {
   
@@ -8,6 +10,8 @@ function App() {
     <BrowserRouter>
       <Routes path="/*">
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,8 +4,8 @@ import { MdAddShoppingCart } from "react-icons/md";
 export default function ProductCard() {
     
     return (
-        <div className="w-[170px] h-fit md:w-[280px] p-1 font-raleway hover:shadow-2xl hover:scale-101 duration-300">
-            <img src="product-img.jpg" className="w-full h-[170px] md:h-[280px] object-cover"  alt="" />
+        <div className="w-[170px] h-fit md:w-[280px] p-1 font-raleway hover:shadow-2xl hover:scale-101 duration-300 rounded-md cursor-pointer">
+            <img src="product-img.jpg" className="w-full h-[170px] md:h-[280px] object-cover" />
             <div className="flex flex-col">
                 <h3 className="text-[12px] font-semibold">Product Name</h3>
                 <div className="flex flex-row gap-[10px] mt-[10px] items-center justify-between">
