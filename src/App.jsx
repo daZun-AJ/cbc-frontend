7800ceb8d26f156613076264fabe69935ce251ef
Dasun <dasuna39@gmail.com>
2025-06-21 10:43:51 +0530

    Add main stylings(colors, fonts) and create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import HomePage from './pages/homePage'
 
 function App() {
   
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes path="/*">
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   )
   
 }
