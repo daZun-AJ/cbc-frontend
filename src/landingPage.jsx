0ee6aee6a9a3a94b65d4870fda009a8fe615a75f
Dasun <dasuna39@gmail.com>
2025-11-09 08:34:19 +0530

    create header component

---
+++ src/landingPage.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header";
+
+
+
+export default function LandingPage() {
+    return (
+        <div className="w-full h-screen flex flex-col p-[10px] items-center">
+            <Header />
+        </div>
+    )
+}
