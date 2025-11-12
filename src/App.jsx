ee5220f9b4785b797b6f35a7060f1ce9f06ca7d4
Dasun <dasuna39@gmail.com>
2025-11-12 06:08:37 +0530

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './landingPage'
+import AdminPage from './adminPage'
+import LoginPage from './loginPage'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Toaster position='top-center' />
       <Routes path="/*">
         <Route path='/*' element={<LandingPage />} />
+        <Route path='/admin/*' element={<AdminPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </BrowserRouter>    
   )
--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -9,9 +9,9 @@ export default function LandingPage() {
     return (
         <div className="w-full h-screen flex flex-col p-[10px] items-center">
             <Header />
-            <ProductCard />
-            <ReviewCard />
-            <Footer />
+
+
+            Home page
         </div>
     )
 }
